c15bdf040c9f991285a7977803631233fa8061ae
Luiz Lopes <luizlopes12@outlook.com>
2021-12-28 14:09:08 -0300

    Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import logo from './img/logo.svg'
+import './style/style.css'
 import './style/bootstrap.min.css'
 let Header = () =>{
     return(
-        <header className='container'>Opa</header>
+        <Fragment>
+        <div className='home-background'>
+        </div>
+        <header className='container'>
+            <div className='row pt-4'>
+                <div className='col'>
+                    <img src={logo}/>
+                </div>
+                <div className='col'>
+                    
+                </div>
+            </div>
+
+
+
+
+        </header>
+        </Fragment>
     );
 }
 export {Header};
